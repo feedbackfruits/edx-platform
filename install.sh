01afcd9399fbb7d1723c133817ac664e5c3492f2
Steffan Sluis <steffansluis@gmail.com>
2014-09-03 00:22:37 +0200

    Sudo in yet a different spot

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo wget https://raw.githubusercontent.com/edx/configuration/master/util/install/vagrant.sh -O - | bash
-sudo cd /var/tmp/configuration/playbooks
-sudo ansible-playbook -i localhost, -c local vagrant-devstack.yml --tags=deploy -e configuration_version=release
-sudo su edxapp
+wget https://raw.githubusercontent.com/edx/configuration/master/util/install/vagrant.sh -O - | bash
+cd /var/tmp/configuration/playbooks
+ansible-playbook -i localhost, -c local vagrant-devstack.yml --tags=deploy -e configuration_version=release
+su edxapp
