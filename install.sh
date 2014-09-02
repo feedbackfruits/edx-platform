e5386aed97b1a1f55b63d6a8e3bb504fa6e4b423
Steffan Sluis <steffansluis@gmail.com>
2014-09-02 21:23:54 +0200

    Moved sudo from yml to sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-wget https://raw.githubusercontent.com/edx/configuration/master/util/install/vagrant.sh -O - | bash
-cd /var/tmp/configuration/playbooks
-ansible-playbook -i localhost, -c local vagrant-devstack.yml --tags=deploy -e configuration_version=release
-su edxapp
+#!/usr/bin/bash
+
+sudo wget https://raw.githubusercontent.com/edx/configuration/master/util/install/vagrant.sh -O - | bash
+sudo cd /var/tmp/configuration/playbooks
+sudo ansible-playbook -i localhost, -c local vagrant-devstack.yml --tags=deploy -e configuration_version=release
+sudo su edxapp
